ad6d16324241a305f4691928e32b6e993d5aa6fd
anvar <anvar@LAPTOP-PKVPCDD8>
2017-11-27 23:11:40 +0530

    Design Mobile

--- papwork/js/app.js
+++ papwork/js/app.js
@@ -8,34 +8,34 @@ var app = angular.module('experienceApp', ['experienceApp.filters', 'experienceA
   }]);
 
 
-  updateZoom = function (index, element) {
-      var $slide = $(element),
-          $index = parseInt($slide.find('.questionPanel').data('question-id')) - 1,
-          distanceFromTopOfViewport = Math.abs($index * $slide.height() * 6 - $(document).scrollTop());
-      //$slide.find('h2').css({
-      //    zoom: 2 - distanceFromTopOfViewport / $slide.height()
-      //});
-      var ratio = distanceFromTopOfViewport / ($slide.height() * $index);
-      $slide.css({
-          zoom: 7 - (ratio <= 1 ? 1 : ratio)
-      });
-      $slide.find('.questionPanel').css({
-          zoom: (20 + (ratio/2)*10)+"%"
-      });
-      $slide.find('.answerContainer').css({
-          opacity: (6 - ratio)/6
-      });
+  //updateZoom = function (index, element) {
+  //    var $slide = $(element),
+  //        $index = parseInt($slide.find('.questionPanel').data('question-id')) - 1,
+  //        distanceFromTopOfViewport = Math.abs($index * $slide.height() * 6 - $(document).scrollTop());
+  //    //$slide.find('h2').css({
+  //    //    zoom: 2 - distanceFromTopOfViewport / $slide.height()
+  //    //});
+  //    var ratio = distanceFromTopOfViewport / ($slide.height() * $index);
+  //    $slide.css({
+  //        zoom: 7 - (ratio <= 1 ? 1 : ratio)
+  //    });
+  //    $slide.find('.questionPanel').css({
+  //        zoom: (20 + (ratio/2)*10)+"%"
+  //    });
+  //    $slide.find('.answerContainer').css({
+  //        opacity: (6 - ratio)/6
+  //    });
 
-      if (ratio < 1) {
-          $slide.addClass('active').removeAttr('style');
-          $slide.prev().addClass('visited').removeClass('active');
-      }
-  };
+  //    if (ratio < 1) {
+  //        $slide.addClass('active').removeAttr('style');
+  //        $slide.prev().addClass('visited').removeClass('active');
+  //    }
+  //};
 
-  $(window).on('load', function () {
-      $(".active + question").each(updateZoom);
-  });
+  //$(window).on('load', function () {
+  //    $(".active + question").each(updateZoom);
+  //});
 
-  $(document).scroll(function () {
-      $(".active + question").each(updateZoom);
-  });
+  //$(document).scroll(function () {
+  //    $(".active + question").each(updateZoom);
+  //});
--- papwork/js/controllers.js
+++ papwork/js/controllers.js
@@ -3,9 +3,19 @@
 
 
 function MyCtrl1($scope, getAllQuestions) {
-    $scope.questions = [];
+    $scope.questionsObj = {
+        questions: [],
+        maxCount: 0,
+        minCount: 0,
+        activeNow: 0,
+        percentComplete: function () {
+            return (this.activeNow / this.maxCount) * 100;
+        }
+    };
     getAllQuestions.then(function (response) {
-        $scope.questions = response.data;
+        $scope.questionsObj.questions = response.data;
+        $scope.questionsObj.activeNow = 1;
+        $scope.questionsObj.maxCount = $scope.questionsObj.questions.length;
     }, function myError(response) {
         $scope.status = response.statusText;
     });
@@ -14,7 +24,7 @@ function MyCtrl1($scope, getAllQuestions) {
 }
 MyCtrl1.$inject = ['$scope', 'getAllQuestions'];
 
- 
+
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
