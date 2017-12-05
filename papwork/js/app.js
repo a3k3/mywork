286577e45c9813c0dfc46117b2da1879c6c2d969
0004062 <0004062@CWT-IND-006>
2017-12-05 20:15:36 +0530

    Minor Updates

--- papwork/js/app.js
+++ papwork/js/app.js
@@ -1,5 +1,5 @@
 // Declare app level module which depends on filters, and services
-var app = angular.module('experienceApp', ['ngRoute', 'experienceApp.filters', 'experienceApp.services', 'experienceApp.directives', 'experienceApp.controllers', 'ngMaterial', 'ngMessages', 'mdCarousel.templates']);
+var app = angular.module('experienceApp', ['ngRoute', 'experienceApp.filters', 'experienceApp.services', 'experienceApp.directives', 'experienceApp.controllers', 'ngMaterial', 'ngMessages']);
 
 app.config(['$routeProvider', '$controllerProvider', '$mdThemingProvider', function ($routeProvider, $controllerProvider, $mdThemingProvider) {
     $routeProvider.when('/experience', { templateUrl: '/partials/experience.html'});
@@ -18,18 +18,15 @@ app.config(['$routeProvider', '$controllerProvider', '$mdThemingProvider', funct
 
 }]);
 
-(function () { angular.module("mdCarousel.templates", []).run(["$templateCache", function ($templateCache) { $templateCache.put("carousel-directive.html", "<div class=\"md-carousel\" >\n\n  <div class=\"slides-container\" layout=\"row\" >\n    <div\n      ng-repeat=\"slideItem in ctrl.cloneData\"\n      class=\"slide\"\n    >\n      <div ng-include=\"ctrl.itemTemplateUrl\" ></div>\n    </div>\n  </div>\n\n  <md-button class=\"md-icon-button left-arrow-button\" >\n    <md-icon ng-click=\"ctrl.navigateLeft()\" >chevron_left</md-icon>\n  </md-button>\n\n  <md-button class=\"md-icon-button right-arrow-button\" >\n    <md-icon ng-click=\"ctrl.navigateRight()\" >chevron_right</md-icon>\n  </md-button>\n\n  <md-radio-group\n    class=\"radio-buttons-container\"\n    layout=\"row\"\n    ng-model=\"ctrl.radioButtonIndex\"\n    layout-align=\"center center\"\n    ng-change=\"ctrl.onRadioButtonClick()\" >\n    <md-radio-button\n      ng-repeat=\"item in ctrl.data\"\n      ng-value=\"$index\"\n      aria-label=\"$index\" >\n    </md-radio-button>\n  </md-radio-group>\n\n</div>\n"); }]); })();
-
-
-  var timeoutId;
-  $(window).bind('mousewheel', function (event) {
-      if (timeoutId) {
-          clearTimeout(timeoutId);
-      }
-      if (event.originalEvent.wheelDelta >= 0) {
-          timeoutId = setTimeout(function () { $('.prev').trigger('click'); }, 300);
-      }
-      else {
-          timeoutId = setTimeout(function () { $('.next').trigger('click'); }, 300);
-      }
-  });
+var timeoutId;
+$(window).bind('mousewheel', function (event) {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    if (event.originalEvent.wheelDelta >= 0) {
+        timeoutId = setTimeout(function () { $('.prev').trigger('click'); }, 300);
+    }
+    else {
+        timeoutId = setTimeout(function () { $('.next').trigger('click'); }, 300);
+    }
+});
