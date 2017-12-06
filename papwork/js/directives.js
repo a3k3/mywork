be5fa26418da0dac8b512578be1b9d732e3c9220
0004062 <0004062@CWT-IND-006>
2017-12-06 19:45:18 +0530

    Minor updates

--- papwork/js/directives.js
+++ papwork/js/directives.js
@@ -2,7 +2,7 @@
 /* http://docs-next.angularjs.org/api/angular.module.ng.$compileProvider.directive */
 
 
-var myapp = angular.module('experienceApp.directives', []);
+var myapp = angular.module('experienceApp.directives', ['ngSanitize']);
 
 myapp.directive('question', function () {
     return {
@@ -45,3 +45,35 @@ function apsUploadFileLink(scope, element, attrs) {
         scope.$apply();
     });
 }
+
+myapp.directive('contenteditable', ['$sce', function($sce) {
+    return {
+        restrict: 'A', // only activate on element attribute
+        require: '?ngModel', // get a hold of NgModelController
+        link: function(scope, element, attrs, ngModel) {
+            if (!ngModel) return; // do nothing if no ng-model
+
+            // Specify how UI should be updated
+            ngModel.$render = function() {
+                element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+                read(); // initialize
+            };
+
+            // Listen for change events to enable binding
+            element.on('blur keyup change', function() {
+                scope.$evalAsync(read);
+            });
+
+            // Write data to the model
+            function read() {
+                var html = element.html();
+                // When we clear the content editable the browser leaves a <br> behind
+                // If strip-br attribute is provided then we strip this out
+                if ( attrs.stripBr && html == '<br>' ) {
+                    html = '';
+                }
+                ngModel.$setViewValue(html);
+            }
+        }
+    };
+}]);
